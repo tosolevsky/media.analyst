3c2d381eba4113f34d3e43396b0b8caeeab4ef72
tosolevsky <batuhantoso@outlook.com.tr>
2025-06-04 21:30:11 +0300

    Update firebase_service.py

--- app/services/firebase_service.py
+++ app/services/firebase_service.py
@@ -1,22 +1,19 @@
 import os
 import json
-import firebase_admin
-from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
 from app.core.logger import logger
 
-logger.info("DEBUG | TESTING = %s", os.getenv("TESTING"))
-
 load_dotenv()
-
 TESTING = os.getenv("TESTING") == "1"
 logger.info("FIREBASE TESTING: %s", TESTING)
 
 if TESTING:
     logger.info("TEST ortamı aktif, Firebase başlatılmayacak.")
     firestore_db = None
-
 else:
+    import firebase_admin
+    from firebase_admin import credentials, firestore
+
     if not firebase_admin._apps:
         try:
             service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
